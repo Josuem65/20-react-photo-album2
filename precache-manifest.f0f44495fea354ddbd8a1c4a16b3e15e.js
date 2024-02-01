dea162953f47dfb910f728560d5419c20f6a3777
Josue Martinez <josue.martinez65@yahoo.com>
2024-01-31 22:32:32 -0800

    Updates

---
+++ precache-manifest.f0f44495fea354ddbd8a1c4a16b3e15e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce04347b4cc24a5337511ec609567920",
+    "url": "/20-react-photo-album2/index.html"
+  },
+  {
+    "revision": "85e97c12a338fcf76749",
+    "url": "/20-react-photo-album2/static/css/main.9f1dbbf3.chunk.css"
+  },
+  {
+    "revision": "9fdfbb7006d8d74a0231",
+    "url": "/20-react-photo-album2/static/js/2.de940c11.chunk.js"
+  },
+  {
+    "revision": "f0ce071822772ea2c1310c56ac76eec8",
+    "url": "/20-react-photo-album2/static/js/2.de940c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949943d9ec7ab9a89470",
+    "url": "/20-react-photo-album2/static/js/3.f900639d.chunk.js"
+  },
+  {
+    "revision": "85e97c12a338fcf76749",
+    "url": "/20-react-photo-album2/static/js/main.a758929d.chunk.js"
+  },
+  {
+    "revision": "e81b7735b264fc40acb0",
+    "url": "/20-react-photo-album2/static/js/runtime-main.c65d00dc.js"
+  }
+]);
